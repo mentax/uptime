f5d4cbe1baae2de9c5e50b58ba529a2232f2c36c
Cesar Canassa <cesar.canassa@gmail.com>
2012-08-08 14:23:18 -0300

    Added Heroku mongo configuration

--- config/production.js
+++ config/production.js
@@ -1,5 +1,9 @@
 module.exports = {
     server: {
-        port: process.env.PORT,
+        port: process.env.PORT
+    },
+
+    mongodb: {
+        connectionString: process.env.MONGOHQ_URL
     }
-}
+};
