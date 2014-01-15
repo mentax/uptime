70958b5c7719d72445630bb383fabafe2b6d238a
Dave <davideweaver@hotmail.com>
2014-01-15 05:35:53 -0500

    added metrics plugin

---
+++ plugins/loggr-metrics/index.js
@@ -0,0 +1,149 @@
+/**
+ * Webhooks plugin
+ *
+ * Notifies all events (up, down, paused, restarted) by sending a
+ * HTTP POST request to the given URL. The request will have a
+ * JSON payload of data from the event
+ *
+ * To enable the plugin, call init() from plugins/index.js
+ *   exports.init = function() {
+ *     require('./webhooks').init();
+ *   }
+ *
+ * Example configuration
+ *   webhooks:
+ *     event:
+ *       up:
+ *         - 'http://localhost:8082'
+ *         - 'http://www.example.com/do/something'
+ *       down:
+ *         - 'http://www.example.com/warn/somebody'
+ *       paused:
+ *       restarted:
+ *     dashboardUrl: 'http://localhost:8082'
+ */
+
+var http = require('http');
+var url = require('url');
+var util = require('util');
+var fs = require('fs');
+var ejs = require('ejs');
+var CheckEvent = require('../../models/checkEvent');
+
+var matchPattern = '^/(.*?)/(g?i?m?y?)$';
+var template = fs.readFileSync(__dirname + '/views/_detailsEdit.ejs', 'utf8');
+
+exports.initWebApp = function(options) {
+  
+    
+    var dashboard = options.dashboard;
+
+    dashboard.on('populateFromDirtyCheck', function(checkDocument, dirtyCheck, type) {
+        //if (type !== 'http' && type !== 'https') return;
+        var match = dirtyCheck.match;
+        if (match) {
+            if (match.indexOf('/') === 0) {
+                throw new Error('Please leave off leading and trailing slashes ' + dirtyCheck.match);
+            }
+            match = '/' + match + '/';
+            var matchParts = match.match(new RegExp(matchPattern));
+            try {
+                // check that the regexp doesn't crash
+                new RegExp(matchParts[1], matchParts[2]);
+            } catch (e) {
+                throw new Error('Malformed regular expression ' + dirtyCheck.match);
+            }
+        }
+        checkDocument.setPollerParam('match', dirtyCheck.match);
+    });
+
+    dashboard.on('checkEdit', function(type, check, partial) {
+        if (type !== 'http' && type !== 'https') return;
+        partial.push(ejs.render(template, { locals: { check: check } }));
+    });
+
+
+    var config = options.config;
+
+    CheckEvent.on('afterInsert', function(checkEvent) {
+        var webhooks = config.webhooks;
+        var hrefs = webhooks.event[checkEvent.message];
+
+        if (!util.isArray(hrefs)) return;
+        checkEvent.findCheck(function(err, check) {
+            var payload = {};
+            if (err) return console.error(err);
+
+            console.log("IN WEBHOOK: " + checkEvent.message);
+            
+            var url = "http://post.loggr.net/1/logs/testlog/events?apikey=8aa2f78930aa4930b6f0657b7fe070e9&text=site%20up&tags=up"
+            
+            var req = http.request(url, function(res) {
+
+            });
+            req.on('error', function(e) {
+              console.log('Problem with webhook request: ' + e.message);
+            });
+            req.end();
+        });
+    });
+    console.log('Enabled loggr-metrics plugin');
+};
+
+exports.initMonitor = function(options) {
+
+    options.monitor.on('pollerPolled', function(check, res, details) {
+        
+        var value = 0;
+
+        var checkPattern = check.pollerParams && check.pollerParams.match;
+        if (checkPattern) {
+            console.log("CHECK REGEX: " + checkPattern);
+            try {
+                var regex = new RegExp(checkPattern);
+                var result = res.body.match(regex);
+                var value = result[1];
+            }
+            catch (e) {
+                value = "ERROR: " + e;
+            }
+        }
+        else {
+            value = res.body;
+            if (value.length > 150)
+                value = value.substring(0, 150);
+        }
+
+        // strip non-numeric
+        value = value.replace(/\D/g, '');
+
+        details.value = {
+            value: value
+        };
+        console.log("LOGGR-METRIC: '" + check.name + "'' VALUE=[" + value + "]");
+
+        var post_data = "apikey=35bd8dfe9aeb4212acb629e26da8469b&value=" + value;
+        var post_options = {
+            host: 'post.loggr.net',
+            port: '80',
+            path: '/1/logs/loggr/metrics/' + check.name,
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/x-www-form-urlencoded',
+              'Content-Length': post_data.length
+            }
+        };
+        // Set up the request
+        var post_req = http.request(post_options, function(res) {
+            res.setEncoding('utf8');
+            res.on('data', function (chunk) {
+                console.log('Response: ' + chunk);
+            });
+        });
+
+        // post the data
+        post_req.write(post_data);
+        post_req.end();
+    });
+
+};
