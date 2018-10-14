8922e793b11a4813976b32e2fff1c4b3df02789f
Michael Lohr <michidk@live.de>
2018-10-14 12:45:19 +0200

    Fixed basic auth plugin by adding basicAuth function

--- plugins/basicAuth/index.js
+++ plugins/basicAuth/index.js
@@ -31,10 +31,34 @@
  */
 var express = require('express');
 
+// from https://github.com/expressjs/express/issues/1991
+/**
+ * Simple basic auth middleware for use with Express 4.x.
+ *
+ * @example
+ * app.use('/api-requiring-auth', utils.basicAuth('username', 'password'));
+ *
+ * @param   {string}   username Expected username
+ * @param   {string}   password Expected password
+ * @returns {function} Express 4 middleware requiring the given credentials
+ */
+basicAuth = function(username, password) {
+  return function(req, res, next) {
+    var user = basicAuth(req);
+
+    if (!user || user.name !== username || user.pass !== password) {
+      res.set('WWW-Authenticate', 'Basic realm=Authorization Required');
+      return res.send(401);
+    }
+
+    next();
+  };
+};
+
 exports.initWebApp = function(options) {
   var config = options.config.basicAuth;
   options.app.on('beforeFirstRoute', function(app, dashboardApp) {
-    app.use(express.basicAuth(config.username, config.password));
+    app.use(basicAuth(config.username, config.password));
   });
 };
 
